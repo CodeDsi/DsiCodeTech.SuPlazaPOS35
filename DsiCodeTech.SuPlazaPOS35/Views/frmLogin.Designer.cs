1244514f771bba7a0b94ec9d78d773986f2c08c9
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2024-01-16 23:55:43 -0600

    se genera diseño de login sin funcionalidad

--- DsiCodeTech.SuPlazaPOS35/Views/frmLogin.Designer.cs
+++ DsiCodeTech.SuPlazaPOS35/Views/frmLogin.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnAuth = new System.Windows.Forms.Button();
+            this.btnDispose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.lblPassword);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 124);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresar Datos del Usuario";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(22, 38);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(22, 76);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(95, 31);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(233, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 68);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(233, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnAuth
+            // 
+            this.btnAuth.Location = new System.Drawing.Point(12, 142);
+            this.btnAuth.Name = "btnAuth";
+            this.btnAuth.Size = new System.Drawing.Size(132, 29);
+            this.btnAuth.TabIndex = 4;
+            this.btnAuth.Text = "&Ingresar";
+            this.btnAuth.UseVisualStyleBackColor = true;
+            // 
+            // btnDispose
+            // 
+            this.btnDispose.Location = new System.Drawing.Point(221, 142);
+            this.btnDispose.Name = "btnDispose";
+            this.btnDispose.Size = new System.Drawing.Size(135, 29);
+            this.btnDispose.TabIndex = 5;
+            this.btnDispose.Text = "&Salir";
+            this.btnDispose.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmLogin";
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(379, 194);
+            this.Controls.Add(this.btnDispose);
+            this.Controls.Add(this.btnAuth);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLogin";
+            this.Text = "Login: Identificacion del Usuario";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAuth;
+        private System.Windows.Forms.Button btnDispose;
     }
 }
