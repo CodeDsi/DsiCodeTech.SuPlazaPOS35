182ef0b285b8600a93ba97daf296df9410172ae1
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2024-01-17 00:37:02 -0600

    se agrega constructor de clase la capa de negocio de usuario

---
+++ DsiCodeTech.Business/UsuarioBusiness.cs
@@ -0,0 +1,35 @@
+﻿using DsiCodeTech.Business.Interface;
+using DsiCodeTech.Common.DataAccess.Infraestructure.Contract;
+using DsiCodeTech.Repository;
+using DsiCodeTech.Repository.Infraestructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.Business
+{
+    public class UsuarioBusiness
+    {
+        #region Declaracion de variables
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UsuarioRepository usuarioRepository;
+        #endregion
+        #region Creacion del Constructor
+        public UsuarioBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            usuarioRepository = new UsuarioRepository(unitOfWork);
+        }
+
+        public UsuarioBusiness()
+        {
+            unitOfWork = new UnitOfWork();
+            usuarioRepository = new UsuarioRepository(unitOfWork);
+            
+        }
+
+        #endregion
+    }
+}
